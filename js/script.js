bd8948202a29943b3ff95f755cd89020a6541848
Nikki Bollman <nikki.bollman@gmail.com>
2021-05-13 20:45:44 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var mainTitle = document.querySelector("h1");
+
+//console.log(mainTitle);
+
+mainTitle.innerText = "Yay, JavaScript!";
+
+var body = document.querySelector("body");
+
+body.style.backgroundColor = "#FEF2F1";
